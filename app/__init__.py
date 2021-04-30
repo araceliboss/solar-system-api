0089bd51d6117e665ba2ca5bc3c6686a40ad8ace
Kristin <Kristineliseboyd13@gmail.com>
2021-04-30 11:49:41 -0700

    solar system

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 
+#db = #CHANGE ME
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.config[""] = False
+    app.config[""] = "postgresql+psycopg2://postgres:postgres@localhost:5432/CHANGE_HERE"
+
+    db.init_app(app)
+    migrate.init__app(app, db)
+    # from we dont know yet 
+    # 
 
     return app
