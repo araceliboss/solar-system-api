2ff1f551574f18ce3aa315f16a3dc7777c5b4877
Araceli <aracelimaldonado4@yahoo.com>
2021-04-30 17:22:08 -0700

    added migrations and ability to create a planet

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,13 @@ from app.models.planet import Planet
 from flask import request, Blueprint, make_response 
 
 planets_bp = Blueprint("planets", __name__)
-@planets_bp.route("/planets", methods=["GET"])
+
+@planets_bp.route("/planets", methods=["POST"])
 def get_planet():
     request_body = request.get_json()
-    new_planet = Planet(title = request_body["title"], description = request_body["description"])
+    new_planet = Planet(name = request_body["name"], description = request_body["description"])
 
     db.session.add(new_planet)
     db.session.commit()
 
-    return make_response(f"Planet {new_planet.title} successfully created", 201)
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
