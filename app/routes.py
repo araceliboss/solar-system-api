8384563f6e78584d60a27f2b76760e39dfae74be
Kristin <Kristineliseboyd13@gmail.com>
2021-05-02 16:36:17 -0700

    completed number 2 of 3

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,31 @@
 from app import db 
 from app.models.planet import Planet 
 from flask import request, Blueprint, make_response 
+from flask import jsonify
 
 planets_bp = Blueprint("planets", __name__)
 
 @planets_bp.route("/planets", methods=["POST"])
-def get_planet():
+def create_planet():
     request_body = request.get_json()
     new_planet = Planet(name = request_body["name"], description = request_body["description"])
 
     db.session.add(new_planet)
     db.session.commit()
 
+
     return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+@planets_bp.route("/planets", methods=["GET"])
+def get_planet():
+    planets = Planet.query.all()
+    planets_response = []
+    
+    for planet in planets:
+        planets_response.append({
+                "id": planet.id,
+                "title": planet.name,
+                "description": planet.description
+            })
+    return jsonify(planets_response)
+
