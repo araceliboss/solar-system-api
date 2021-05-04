c4a0ab2be28a969ac6e6c9ccbc98403562f75c5a
Kristin <Kristineliseboyd13@gmail.com>
2021-05-04 15:50:06 -0700

    most recent file, updated test_no_planet_data

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,21 @@ def test_get_all_planets_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+
+def test_get_one_planet(client):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": "",
+        "name": "",
+        "description": ""
+    }
+
+def test_no_planet_data(client):
+    response = client.handle_planet("/planets/1")
+    assert response.status_code == 404
