0ab8a7083ba202f1169150d9bd8318a160cdf3fd
Kristin <Kristineliseboyd13@gmail.com>
2021-05-04 15:31:11 -0700

    Actually installed the correct stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,20 @@
 import pytest
+from app import create_app
+from app import db
+
 
 @pytest.fixture
-def empty_list():
-    return []
+def app():
+    app = create_app({"TESTING": True})
 
-def test_len_of_empty_list(empty_list):
-    assert isinstance(empty_list, list)
-    assert len(empty_list) == 0
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
